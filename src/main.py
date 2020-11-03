67f20c5a3812a9ad146838f5041467a8a9ff8077
Benedikt Kleinmeier <benedikt.kleinmeier@hm.edu>
2020-11-03 21:48:49 +0100

    In "main.py", try out initial steps to load a PDF file using "fitz" package and to render a page as SVG

--- src/main.py
+++ src/main.py
@@ -1,2 +1,24 @@
+import argparse
+import fitz
+
+test_file = "../resources/test.pdf"
+
+def parse_command_line_arguments():
+    parser = argparse.ArgumentParser(description="pdfedit - a free an open source PDF editor")
+
+    parser.add_argument("files", type=str, nargs="*",
+                        help="Open the given file(s)")
+
+    return parser.parse_args()
+
 if __name__ == "__main__":
-    print("Hello World!")
+    args = parse_command_line_arguments()
+    
+    doc = fitz.open(test_file)
+    
+    if doc.pageCount > 0:
+        page = doc.loadPage(0);
+        
+        txt = page.getSVGimage()
+        with open("test.svg", "w") as file:
+            file.write(txt)
