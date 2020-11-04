a888fec15c91d0a1c7d5d29a784875e5d21f9a64
Benedikt Kleinmeier <benedikt.kleinmeier@hm.edu>
2020-11-04 21:59:34 +0100

    Convert first PDF file to SVG and render it in a window using Qt's "QSvgWidget"

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 import argparse
 import fitz
 
-test_file = "../resources/test.pdf"
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtSvg import QSvgWidget
+
+test_pdf_input = "../resources/inkscape.pdf"
+test_svg_output = "test.svg"
 
 def parse_command_line_arguments():
     parser = argparse.ArgumentParser(description="pdfedit - a free an open source PDF editor")
@@ -11,14 +15,32 @@ def parse_command_line_arguments():
 
     return parser.parse_args()
 
-if __name__ == "__main__":
-    args = parse_command_line_arguments()
-    
-    doc = fitz.open(test_file)
+def convert_pdf_to_svg(pdffile, filename):
+    doc = fitz.open(pdffile)
     
     if doc.pageCount > 0:
         page = doc.loadPage(0);
         
-        txt = page.getSVGimage()
-        with open("test.svg", "w") as file:
-            file.write(txt)
+        svg_string = page.getSVGimage()
+        return svg_string
+    else:
+        return None
+
+def run_qt_application(svg_string):
+    app = QApplication([])
+    
+    svg_widget = QSvgWidget()
+    svg_widget.renderer().load(bytearray(svg_string, encoding="utf-8"))
+    svg_widget.setGeometry(100, 100, 500, 500)
+    svg_widget.show()
+    
+    app.exec_()
+
+if __name__ == "__main__":
+    args = parse_command_line_arguments()
+    
+    svg_string = convert_pdf_to_svg(test_pdf_input, test_svg_output)
+    
+    if svg_string != None:
+        # print(svg_string)
+        run_qt_application(svg_string)
