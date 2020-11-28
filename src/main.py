280c5b4d90b11339d54e0ede79cdf1611effc06f
Benedikt Kleinmeier <benedikt.kleinmeier@hm.edu>
2020-11-28 17:59:25 +0100

    Minimal working version to open a PDF file and render it in the central widget as SVG
    Add following shortcuts:
    - Ctrl + O: Open PDF file
    - Ctrl + Q: Quit application

--- src/main.py
+++ src/main.py
@@ -3,10 +3,7 @@ import fitz
 
 from PyQt5 import uic
 from PyQt5.QtSvg import QSvgWidget
-from PyQt5.QtWidgets import QApplication
-
-test_pdf_input = "../resources/inkscape.pdf"
-test_svg_output = "test.svg"
+from PyQt5.QtWidgets import QApplication, QFileDialog
 
 def parse_command_line_arguments():
     parser = argparse.ArgumentParser(description="pdfedit - a free an open source PDF editor")
@@ -16,38 +13,49 @@ def parse_command_line_arguments():
 
     return parser.parse_args()
 
-def convert_pdf_to_svg(pdffile, filename):
-    doc = fitz.open(pdffile)
-    
+def run_qt_application():
+    # Create GUI elements
+    Form, Window = uic.loadUiType("./gui/main_window.ui")
+
+    app = QApplication([])
+    window = Window()
+    form = Form()
+    form.setupUi(window)
+    svg_widget = QSvgWidget()
+
+    # Configure GUI elements
+    window.setCentralWidget(svg_widget)
+    configure_qt_signals_and_slots(form, window, svg_widget)
+
+    # Show the GUI
+    window.show()
+    app.exec_()
+
+def configure_qt_signals_and_slots(form, window, svg_widget):
+    form.action_open.triggered.connect(lambda: open_file_dialog(window, svg_widget))
+
+def open_file_dialog(parent, svg_widget):
+    # TODO: Use "tr()" to translate strings
+    (filename, selected_filter) = QFileDialog.getOpenFileName(parent, "Open PDF File", "~", "PDF Files (*.pdf)")
+
+    if filename:
+        svg_string = convert_pdf_to_svg(filename)
+
+        if svg_string != None:
+            svg_widget.renderer().load(bytearray(svg_string, encoding="utf-8"))
+
+def convert_pdf_to_svg(filename):
+    doc = fitz.open(filename)
+
     if doc.pageCount > 0:
         page = doc.loadPage(0);
-        
+
         svg_string = page.getSVGimage()
         return svg_string
     else:
         return None
 
-def run_qt_application(svg_string):
-    Form, Window = uic.loadUiType("./gui/main_window.ui")
-
-    app = QApplication([])
-
-    window = Window()
-    form = Form()
-    form.setupUi(window)
-    window.show()
-
-    svg_widget = QSvgWidget()
-    svg_widget.renderer().load(bytearray(svg_string, encoding="utf-8"))
-
-    window.setCentralWidget(svg_widget)
-
-    app.exec_()
-
 if __name__ == "__main__":
     args = parse_command_line_arguments()
-    
-    svg_string = convert_pdf_to_svg(test_pdf_input, test_svg_output)
-    
-    if svg_string != None:
-        run_qt_application(svg_string)
+
+    run_qt_application()
